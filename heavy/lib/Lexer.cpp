1f8b6d715a92eaccd91c7b4cc10646b34ea9e4b8
Jason Rice <ricejasonf@gmail.com>
2021-03-15 13:01:08 -0700

    [Heavy] Adds support for numbers in identifiers

--- heavy/lib/Lexer.cpp
+++ heavy/lib/Lexer.cpp
@@ -32,6 +32,8 @@ namespace {
     case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': case 'n':
     case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u':
     case 'v': case 'w': case 'x': case 'y': case 'z':
+    case '0': case '1': case '2': case '3': case '4':
+    case '5': case '6': case '7': case '8': case '9':
     case '+': case '-': case '.': case '*': case '/': case '<': case '=':
     case '>': case '!': case '?': case ':': case '$': case '%': case '_':
     case '&': case '~': case '^':
